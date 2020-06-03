1a5a4f61f9f4eb9119d1ec40b084ab16d8c994b7
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-06-03 01:41:45 -0400

    persistencia a reloads

--- lib/src/bloc/Provider.dart
+++ lib/src/bloc/Provider.dart
@@ -3,9 +3,20 @@ import 'package:validacion_de_formulario/src/bloc/Logic_bloc.dart';
 export 'package:validacion_de_formulario/src/bloc/Logic_bloc.dart';
 
 class Provider extends InheritedWidget{
+
+  static Provider _instancia;
+
+  factory Provider ({Key key, Widget child}){
+    if( _instancia == null){
+      _instancia = new Provider._internal(key:key, child:child);
+    }
+    return _instancia;
+  }
+
+
   final loginBloc = LogicBloc();
 
-  Provider({ Key key,Widget child}):super(key: key,child : child);
+  Provider._internal({ Key key,Widget child}):super(key: key,child : child);
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
