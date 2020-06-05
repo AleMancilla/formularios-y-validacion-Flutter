45392c7b8de4418a7a58d947b854b405b844eee4
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-06-04 22:37:15 -0400

    validacion de formulario

--- lib/pages/producto_pages.dart
+++ lib/pages/producto_pages.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:validacion_de_formulario/utils/utils.dart'as utils;
 
-class ProductoPage extends StatelessWidget {
-  const ProductoPage({Key key}) : super(key: key);
+class ProductoPage extends StatefulWidget {
+  
+  
 
+  @override
+  _ProductoPageState createState() => _ProductoPageState();
+}
+
+class _ProductoPageState extends State<ProductoPage> {
+  final formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +23,7 @@ class ProductoPage extends StatelessWidget {
         child: Container(
           padding: EdgeInsets.all(15.0),
           child: Form(
+            key: formkey,
             child: Column(
               children: <Widget>[
                 _crearNombre(),
@@ -34,6 +43,13 @@ class ProductoPage extends StatelessWidget {
       decoration: InputDecoration(
         labelText: 'Producto'
       ),
+      validator: (value){
+        if(value.length <3){
+          return 'Ingrese el nombre del producto';
+        }else{
+          return null;
+        }
+      },
     );
   }
 
@@ -43,12 +59,19 @@ class ProductoPage extends StatelessWidget {
       decoration: InputDecoration(
         labelText: 'Producto'
       ),
+      validator: (value){
+        if(utils.isNumeric(value)){
+          return null;
+        }else{
+          return "Solo Numeros";
+        }
+      },
     );
   }
 
   _crearBoton(){
     return RaisedButton.icon(
-      onPressed: (){}, 
+      onPressed: _submit(), 
       icon: Icon(Icons.save), 
       textColor: Colors.white,
       label: Text("Guardar"),
@@ -56,6 +79,12 @@ class ProductoPage extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20.0)
       ),
+      
     );
+  }
+  _submit(){
+    if(!formkey.currentState.validate()) return;
+
+    print("Todo Ok");
   }
 }
