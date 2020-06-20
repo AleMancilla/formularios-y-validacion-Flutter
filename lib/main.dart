0e60a3d5dea617f555f096eebc2b1dd80bb5971a
Alejandro <alejandro.mancilla.umsa@gmail.com>
2020-06-20 01:02:06 -0400

    preferencias de usuario guardando token

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,22 @@ import 'package:validacion_de_formulario/src/pages/Login_Page.dart';
 import 'package:validacion_de_formulario/src/pages/Registro_Page.dart';
 import 'package:validacion_de_formulario/src/pages/producto_pages.dart';
 import 'package:validacion_de_formulario/src/bloc/Provider.dart';
+import 'package:validacion_de_formulario/src/preferencias_usuario/Preferencias_usuario.dart';
  
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = new PreferenciasUsuario();
+  await prefs.initPrefs();
+  runApp(MyApp());
+} 
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final prefs = new PreferenciasUsuario();
+    print(prefs.token);
+
     return Provider(
       child: MaterialApp(
         title: 'Material App',
--- lib/src/providers/UsuarioProvider.dart
+++ lib/src/providers/UsuarioProvider.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
 import 'package:http/http.dart'as http;
+import 'package:validacion_de_formulario/src/preferencias_usuario/Preferencias_usuario.dart';
 class UsuarioProvider{
 
   final String _firebaseToken = 'AIzaSyAUsGYun0_Emv91oW7hG9Ax9ZEA_9dnfO0';
+  final _prefs = new PreferenciasUsuario();
 
   Future<Map<String,dynamic>> nuevoUsuario(String email, String pasword)async{
     final autData ={
@@ -21,6 +23,9 @@ class UsuarioProvider{
     print(decodeResp);
 
     if(decodeResp.containsKey('idToken')){
+
+      _prefs.token = decodeResp['idToken'];
+      
       return { 'ok':true , 'token':decodeResp['idToken']};
     }else{
       return { 'ok':false , 'token':decodeResp['error']['message']};
@@ -44,6 +49,10 @@ class UsuarioProvider{
     print(decodeResp);
 
     if(decodeResp.containsKey('idToken')){
+
+      
+      _prefs.token = decodeResp['idToken'];
+
       return { 'ok':true , 'token':decodeResp['idToken']};
     }else{
       return { 'ok':false , 'token':decodeResp['error']['message']};
